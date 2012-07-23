16df0f3399c0fc6d6383a726ec6104715013a188
Babs Gosgens <babs@crossinghippos.nl>
2012-07-23 09:55:09 +0200

    Erm... forgot the attchment ;)

---
+++ header.php
@@ -0,0 +1,28 @@
+		<div>
+			<h1><a href="index.php"><span><strong>Molajo</strong> Admin Interface</span></a></h1>
+			<nav>
+				<dl class="settings">
+					<dt><a href="#search"><i>=</i><span>Search</span></a></dt
+					><dd id="search">
+						<form role="search">
+							<fieldset>
+								<input type="search" placeholder="Search Resources">
+							</fieldset>
+						</form>
+					</dd
+					><dt class="user"><a href="#user"><img src="media/smile.png" width="40" height="40" /><span>Babs G&ouml;sgens</span></a></dt
+					><dd id="user">
+						<ul>
+							<li><a href="#">Dropdown Item</a></li>
+							<li><a href="#">Another Dropdown Item</a></li>
+							<li class="divider"></li>
+							<li><a href="#">Last Item</a></li>
+						</ul>
+					</dd
+					><dt class="last"><a href="#settings"><i>a</i><span>Settings</span></a></dt
+					><dd id="settings">
+						<a href="" class="close"><i></i><span>Close</span></a>
+					Settings</dd>
+				</dl>
+			</nav>
+		</div>
