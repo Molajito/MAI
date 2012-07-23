c03d10279cde3e7d72264d52a3ced40aa7940019
Babs Gosgens <babs@crossinghippos.nl>
2012-07-23 16:40:46 +0200

    Tmp styling close button

--- _header.php
+++ _header.php
@@ -4,6 +4,7 @@
 				<dl class="settings">
 					<dt><a href="#search"><i>=</i><span>Search</span></a></dt
 					><dd id="search">
+						<a href="#" class="dismiss"><i>g</i><span>Close</span></a>
 						<form role="search">
 							<fieldset>
 								<input type="search" placeholder="Search Resources">
@@ -12,6 +13,7 @@
 					</dd
 					><dt class="user"><a href="#user"><img src="media/smile.png" width="40" height="40" /><span>Babs G&ouml;sgens</span></a></dt
 					><dd id="user">
+						<a href="#" class="dismiss"><i>g</i><span>Close</span></a>
 						<ul>
 							<li><a href="#">Dropdown Item</a></li>
 							<li><a href="#">Another Dropdown Item</a></li>
@@ -21,7 +23,7 @@
 					</dd
 					><dt class="last"><a href="#settings"><i>a</i><span>Settings</span></a></dt
 					><dd id="settings">
-						<a href="" class="close"><i></i><span>Close</span></a>
+						<a href="#" class="dismiss"><i>g</i><span>Close</span></a>
 					Settings</dd>
 				</dl>
 			</nav>
