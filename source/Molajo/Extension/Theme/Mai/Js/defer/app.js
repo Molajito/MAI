d87cbf64cd79e936b377bab980a33848e3a94cdc
Babs Gosgens <babs@crossinghippos.nl>
2012-08-03 21:34:51 +0200

    Move theme files to Theme folder

--- source/Molajo/Extension/Theme/Mai/Js/defer/app.js
+++ source/Molajo/Extension/Theme/Mai/Js/defer/app.js
@@ -1,38 +1,21 @@
-jQuery(document).ready(function ($) {
+(function ($) {  
 
-    /* Use this js doc for all application specific JS */
-
-    /* TABS --------------------------------- */
-    /* Remove if you don't need :) */
-
-    function activateTab($tab) {
-        var $activeTab = $tab.closest('dl').find('dd.active'),
-            contentLocation = $tab.children('a').attr("href") + 'Tab';
-
-        console.log($activeTab );
-
-        // Strip off the current url that IE adds
-        contentLocation = contentLocation.replace(/^.+#/, '#');
-
-        //Make Tab Active
-        $activeTab.removeClass('active');
-        $tab.addClass('active');
-
-        //Show Tab Content
-        $(contentLocation).closest('.tabs-content').children('li').hide();
-        $(contentLocation).css('display', 'block');
-    }
-
-    $('dl.tabs dd a').on('click.fndtn', function (event) {
-        activateTab($(this).parent('dd'));
-    });
-
-    alert(window.location.hash);
-
-
-    if (window.location.hash) {
-        activateTab($('a[href="' + window.location.hash + '"]'));
-        $.foundation.customForms.appendCustomMarkup();
-    }
-
-});
+  $(function(){
+    $(document).foundationAlerts();
+    $(document).foundationButtons();
+    $(document).foundationAccordion();
+    $(document).foundationNavigation();
+    $(document).foundationCustomForms();
+    $(document).foundationTabs({callback:$.foundation.customForms.appendCustomMarkup});
+    
+    $(document).tooltips();
+    $('input, textarea').placeholder();
+    
+    // UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
+    // $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'left'});
+    // $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'left'});
+    // $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'left'});
+    // $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'left'});
+  });
+  
+})(jQuery);
